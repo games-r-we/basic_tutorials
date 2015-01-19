726d8f91efc736d62f5b10854ec66282f45fbe42
NathanGCoats <natec@plumbersstock.com>
2015-01-18 23:34:59 -0700

    Fixes

--- c++/examples/hello_world.cpp
+++ c++/examples/hello_world.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 using namespace std;
 
-// main() is where program execution begins.
+int test(){
+  int a = 0;
+  int b  = 5;
+  int c = a + b;
+  return c;
+}
 
+boolean test2(){
+  boolean a = true;
+  boolean b = false;
+  return a && b;
+}
+
+// main() is where program execution begins.
 int main(){
 	//cout output a line of text.  \n means new line.
-   cout << "Hello World \n";
+   //cout << "Hello World \n";
    //because the main type is int  (int main() ) must return an integer.
+   int get_test = test();
+   cout << get_test << "\n";
    return 0;
 }
